e64aff998e201662a2d9fd1f05f9b371baed7d23
shaunjacob <46585856+shaunjacob@users.noreply.github.com>
2022-01-14 10:34:28 +1100

    Updated main.tf, removed resourcegroups Created resourcegroups.tf added terraform readme

--- AVS-Landing-Zone/GreenField/Terraform/main.tf
+++ AVS-Landing-Zone/GreenField/Terraform/main.tf
@@ -11,6 +11,10 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
+}
+
 ## Optional settings to setup a terraform backend in Azure storage
 
 # terraform {
@@ -21,25 +25,3 @@ terraform {
 #         key = "terraform.tfstate"
 #     }
 # }
-
-provider "azurerm" {
-  features {}
-}
-
-# Create the AVS Private Cloud Resource Group
-resource "azurerm_resource_group" "privatecloud" {
-  name     = "${var.prefix}-PrivateCloud"
-  location = "${var.region}"
-}
-
-# Create the Jumpbox Resource Group
-resource "azurerm_resource_group" "jumpbox" {
-  name     = "${var.prefix}-Jumpbox"
-  location = "${var.region}"
-}
-
-# Create Network Resource Group
-resource "azurerm_resource_group" "network" {
-  name     = "${var.prefix}-Network"
-  location = "${var.region}"
-}
