fb37b9223d4de43ed953fc7a640c0f92f9e92da3
jchancellor-ms <jchancellor@microsoft.com>
2022-02-11 15:27:20 -0800

    added terraform with ARM template deployment

--- BrownField/Networking/AVS-to-OnPremises-ExpressRoute-GlobalReach/Terraform/main.tf
+++ BrownField/Networking/AVS-to-OnPremises-ExpressRoute-GlobalReach/Terraform/main.tf
@@ -1,2 +1,35 @@
 #The provider doesn't have an existing configuration here yet.  Confirm provider update status
 #Is possible to call the ARM API template directly, but doesn't allow for destroy or tracking
+locals {
+  deploymentName = "${var.PrivateCloudName}-${var.ExpressRouteId}-${random_string.namestring.result}"
+}
+
+resource "random_string" "namestring" {
+  length  = 4
+  special = false
+  upper   = false
+  lower   = true
+}
+
+
+resource "azurerm_resource_group_template_deployment" "avsGlobalReach" {
+  name                = local.deploymentName
+  resource_group_name = var.DeploymentResourceGroupName
+  deployment_mode     = "Incremental"
+
+  parameters_content = jsonencode({
+    "PrivateCloudName" = {
+      value = var.PrivateCloudName
+    },
+    "ExpressRouteAuthorizationKey" = {
+      value = var.ExpressRouteAuthorizationKey
+    },
+    "ExpressRouteId" = {
+      value = var.ExpressRouteId
+    }
+
+  })
+
+  template_content = file(AVSGlobalReach.deploy.json)
+
+}
--- BrownField/Networking/AVS-to-OnPremises-ExpressRoute-GlobalReach/Terraform/vars.tf
+++ BrownField/Networking/AVS-to-OnPremises-ExpressRoute-GlobalReach/Terraform/vars.tf
@@ -1,10 +1,10 @@
-variable "expressRouteAuthorizationKey" {
+variable "ExpressRouteAuthorizationKey" {
   type        = string
   sensitive   = true
   description = "The Express Route Authorization Key to be redeemed by the connection"
 }
 
-variable "expressRouteId" {
+variable "ExpressRouteId" {
   type        = string
   sensitive   = true
   description = "The Express Route ID to create the connection to"
@@ -13,4 +13,9 @@ variable "expressRouteId" {
 variable "PrivateCloudName" {
   type        = string
   description = "The name of the existing Private Cloud that should be used for the connection"
+}
+
+variable "DeploymentResourceGroupName" {
+  type        = string
+  description = "Resource Group where the new globalReach resource will be created."
 }
