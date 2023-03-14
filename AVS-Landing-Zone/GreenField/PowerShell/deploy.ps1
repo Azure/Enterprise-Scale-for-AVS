012e6dbb1664e6ec9eb96293e79313ab2c45de37
Fletcher Kelly <fletcher_kelly@live.co.za>
2023-03-14 07:40:53 +0000

    flattening structure

--- AVS-Landing-Zone/GreenField/PowerShell/deploy.ps1
+++ AVS-Landing-Zone/GreenField/PowerShell/deploy.ps1
@@ -8,7 +8,7 @@ Write-Output "Reading variables"
 #$variables = Get-Content ..\PowerShell\variables\variables.json | ConvertFrom-Json
 & "$rootFolder\variables\set-variables.ps1"
 #.\variables\set-variables.ps1
-#$variables
+$variables
 
 # Deploy Resource Groups
 Write-Output "Deploying Resource Groups now"
@@ -19,12 +19,14 @@ Write-Output "Deploying Private Cloud now"
 & "$rootFolder\2.private-cloud\deploy-withjson.ps1"
 #. .\2.private-cloud\deploy-withjson.ps1
 
+& "$rootFolder\2.private-cloud\deploymentcheck.ps1"
+
 # Deploy Networking
 Write-Output "Deploying Networking now"
 #. .\3.network\deploy-withjson.ps1
-& "$rootFolder\3.network\deploy-withjson.ps1"
+#& "$rootFolder\3.network\deploy-withjson.ps1"
 
 # Deploy Jumpbox
 write-Output "Deploying Jumpbox now"
-& "$rootFolder\4.jumpbox\deploy-withjson.ps1"
+#& "$rootFolder\4.jumpbox\deploy-withjson.ps1"
 #. .\4.jumpbox\deploy-withjson.ps1
